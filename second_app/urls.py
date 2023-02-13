4a111e38c166fc5778af2b275a7874b132ff6f85
ForoozanSafari <forozansafari@gmail.com>
2023-02-13 20:25:39 +0330

    second commit

--- second_app/urls.py
+++ second_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.shortcuts import render
 
 def login(request):
-    return render(request,'second_app\login ')
+    return render(request,'second_app\login ')# 
